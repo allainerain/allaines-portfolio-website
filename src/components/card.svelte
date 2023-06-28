82c4b0d925b3cf3810e385a538382c3073e0c85e
allainerain <allainericci@gmail.com>
2023-06-28 14:42:44 +0800

    hero and section

---
+++ src/components/card.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<div class="card">
+    <div class="container">
+      <h4>Mask Patrol</h4>
+      <p>A Face Mask Detection model using Computer Vision and Artificial Intelligence.</p>
+    </div>
+    <img src="card-photos/placeholder.jpg" alt="Image Description" style="width:100%"/>
+</div>
+
+<style>
+
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  background-color: #161A11;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px; /* 5px rounded corners */
+}
+
+/* Add rounded corners to the top left and the top right corner of the image */
+img {
+  border-radius: 5px 5px 0 0;
+}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,77 @@
+<script>
+    import Card from "../components/card.svelte";
+</script>
+
+<body>
+    <section class="Hero">
+        <h1>Designer at Sketch. Having fun cycling around.</h1>
+        <p>Hey, I’m Allaine -- a Computer Science undergraduate from the University of the Philippines. I dabble in Artificial Intelligence, Data Science and UI/UX Design. </p>
+    </section>
+
+    <section class="AI">
+        <h1>AI and Data Science Journey</h1>
+        <p>Something has brought me to discover more about data science. These are the projects that I have made so far More story to come, kind of narrative style. These words are just a placeholder for the future.</p>
+        <div class="cards">
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </div>
+    </section>
+</body>
+
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Gentium+Plus:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+body {
+    margin: 0;
+    width: 100%;
+    background-color: #0D0F0A;
+}
+
+section {
+    color: #D9D9D9;
+    width: 800px;
+    margin: 150px auto 150px auto;
+    text-align: center;
+}
+
+h1{
+    font-family: 'Gentium Plus', serif;
+    font-weight: 400;
+    font-size: 96px;
+    line-height: 1.11;
+}
+
+.AI h1 {
+    font-size: 64px;
+}
+
+p{
+    font-family: 'Source Sans 3', sans-serif;
+    font-size: 20px;
+}
+
+.cards {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 50px;
+    justify-items: center;
+}
+
+@media only screen and (max-width: 900px) {
+    section{
+        width: 100%;
+    }
+
+}
+
+@media only screen and (max-width:770px){
+    h1{
+        font-size: 86px;
+    }
+
+}
+</style>
