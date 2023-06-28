93c5f1854fe0098819cdef1e95f59bb1150beab6
allainerain <allainericci@gmail.com>
2023-06-28 17:23:29 +0800

    Added navbar

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -18,7 +18,6 @@
 </a>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Gentium+Plus:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Mono&family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 h4{
     font-family: 'Source Sans 3', sans-serif;
