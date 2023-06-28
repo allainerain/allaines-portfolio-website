99ec65fb9ed9f451dd76406fb1687ae6b79f3f5c
allainerain <allainericci@gmail.com>
2023-06-28 17:45:09 +0800

    Feature: Added footer component

---
+++ src/components/footer.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<section>
+    Made with Love
+
+    <div class="links">
+        <a href="https://www.linkedin.com/in/allaine-tan/" target="_blank"> LinkedIn </a>
+        <a href="https://github.com/allainerain" target="_blank"> Github </a>
+        <a href="mailto:allainericci@gmail.com" target="_blank"> Email </a>
+    </div>
+</section>
+
+<style>
+
+section {
+    display: flex;
+    justify-content: space-between;
+    font-family: 'Source Sans 3', sans-serif;
+    color: #838481;
+}
+
+section .links {
+    text-align: right;
+    margin: 0;
+}
+
+a{
+    text-decoration: none;
+    margin: 0 10px;
+    color: #838481;
+    transition: color 0.3s ease; 
+}
+
+a:hover{
+    color:  #C2C2C2;
+}
+
+</style>
