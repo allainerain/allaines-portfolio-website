9851dac6bb9c9209aab23c4227430268eb4b09b1
allainerain <allainericci@gmail.com>
2023-07-03 14:30:05 +0800

    feat: integrated notion

---
+++ src/routes/blog/+server.js
@@ -0,0 +1,10 @@
+import { getBlog } from '../../services/notion.server.js';
+import { json } from '@sveltejs/kit';
+
+export async function GET() {
+  const blog = await getBlog();
+  console.log("FROM PAGE SERVER");
+  console.log(blog);
+
+  return json(blog);
+}
