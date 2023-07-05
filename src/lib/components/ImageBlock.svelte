6113dd35e9844f2655e2cae39eb2a3e1b21eae46
allainerain <allainericci@gmail.com>
2023-07-05 21:17:52 +0800

    UI: dynamic image rendering

--- src/lib/components/ImageBlock.svelte
+++ src/lib/components/ImageBlock.svelte
@@ -1,19 +1,18 @@
 <script>
-	import { Image } from "@unpic/svelte";
+    import { Image } from "@unpic/svelte";
     export let block;
 </script>
 
 {#if block.image}
     {#if block.image.type=="file"}
-        <Image src={block.image.file.url} layout="fullWidth" aspectRatio={16/9} class="aspect-video object-cover rounded-md" />
+        <Image src={block.image.file.url} layout="fullWidth" aspectRatio={16/9} style="border-radius: 10px; max-width: 100%;" class="aspect-video object-cover" />
         {#if block.image.caption?.length > 0}
-            <p class="w-full text-xs text-gray-400 text-center">{block.image.caption?.[0].plain_text}</p>
+            <p style="width: 100%; font-size: 0.625rem; color: #888888; text-align: center;">{block.image.caption?.[0].plain_text}</p>
         {/if}
     {:else if block.image.type=="external"}
-        <Image src={block.image.external.url} layout="fullWidth" aspectRatio={16/9} class="aspect-video object-cover rounded-md" />
+        <Image src={block.image.external.url} layout="fullWidth" aspectRatio={16/9} style="border-radius: 10px; max-width: 100%;" class="aspect-video object-cover" />
         {#if block.image.caption?.length > 0}
-            <p class="w-full text-xs text-gray-400 text-center">{block.image.caption?.[0].plain_text}</p>
+            <p style="width: 100%; font-size: 0.625rem; color: #888888; text-align: center;">{block.image.caption?.[0].plain_text}</p>
         {/if}
     {/if}
 {/if}
-
