733f28f068ca6c3baaa7970458e3317a770fdb10
allainerain <allainericci@gmail.com>
2024-02-08 20:30:49 +0800

    style: callout format and text parsing

--- src/lib/components/Callout.svelte
+++ src/lib/components/Callout.svelte
@@ -10,7 +10,12 @@
 </script>
 
 <body>
-    {emoji} {title}
+    <p>
+        {emoji} 
+        <span class="title"> {title} </span>
+    </p>
+    
+    
 
     {#if children}
         {#each children as child}
@@ -25,6 +30,19 @@
     @import url('https://fonts.googleapis.com/css2?family=Gentium+Plus:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Mono&family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
     body{
-        background-color: grey;
+        background: #181818;
+        padding: 24px;
+        border-radius: 5px;
+        border: 1.5px solid #292929;
+        margin: 0;
+        margin-top: 16px;
     }
+
+    .title{
+        padding: 8px;
+        font-family: 'Roboto Mono', monospace;
+        font-size: 14px;
+        color: #838481;
+    }
+
 </style>
