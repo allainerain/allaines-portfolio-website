9366e41296a5128e4d26f573d317743e7f97ed54
allainerain <allainericci@gmail.com>
2024-06-17 23:30:13 +0800

    style: adjust the style of the theme switch and the navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,72 +1,39 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+    import ThemeSwitch from "./ThemeSwitch.svelte";
+    let isDark = true
+    const dispatch = createEventDispatcher()
 
+    function toggleMode() {
+        isDark = !isDark
+        console.log(isDark)
+        dispatch('changeMode', {
+            mode: isDark
+        });
+    }
 </script>
 
-<section class="navbar">
-    <div class="pill">
-        <a href="/"> Home </a>
-        <a href="/about"> About </a>
-        <img src="../icons/diamond.png" alt="Website Icon">
-        <a href="/works"> Works </a>
-        <a href="/blog"> Blog </a>
+<div class="
+    flex fixed w-full items-center justify-center h-[100px] z-50 pt-8
+    md:top-0
+    max-md:bottom-0 max-md:left-0 max-md:right-0 
+    bg-gradient-to-t from-light-background to-transparent
+    dark:from-dark-background to-transparent
+    md:bg-gradient-to-b
+">
+    <div class="
+        inline-flex gap-4 bg-light-background items-center justify-center border m-4 p-4 rounded-lg border-1 border-dark-accent/[0.2]
+        max-md:w-[80%] max-md:ml-[5%]
+        dark:bg-dark-background dark:text-dark-text
+    ">
+        <a href="/" class="px-3 py-1"> Home </a>
+        <a href="/about" class="px-3 py-1"> About </a>
+        <!-- <button on:click={toggleMode} class="px-3 py-1">
+            <img src="../icons/diamond.png" alt="Website Icon" class="w-6 h-6">
+        </button> -->
+        <ThemeSwitch />
+
+        <a href="/works" class="px-3 py-1"> Works </a>
+        <a href="/blog" class="px-3 py-1"> Blog </a>
     </div>
-</section>
-
-<style>
-
-img{
-    margin: 0px 25px;
-    height: 40px;
-    width: 40px;
-}
-section {
-    position: sticky;
-    top: 0;
-    text-align: center;
-    height: 120px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: linear-gradient(to bottom, #0D0F0A, transparent);
-    z-index: 999; /* Add a high z-index value */
-}
-
-.navbar > .pill {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 10px;
-    padding: 10px;
-    background-color: #0D0F0A;
-    outline-style: solid;
-    outline-color: #1C1C1C;
-    outline-width: 2px;
-}
-
-.navbar a {
-    font-family: 'Hogbine', sans-serif;
-    margin: 0 20px;
-    color: white;
-    text-decoration: none;
-    font-size: 16px;
-}
-
-/* PHONE VIEW */
-@media only screen and (max-width: 770px) {
-    section {
-        position: fixed;
-        top: auto;
-        bottom: 0;
-        background: linear-gradient(to top, #0D0F0A, transparent);
-        /* background: white */
-    }
-    .navbar a{
-        margin: 0 10px;
-    }
-    img{
-        margin: 0px 20px;
-
-    }
-}
-</style>
+</div>
