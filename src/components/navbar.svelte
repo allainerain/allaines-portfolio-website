210875aeb7e7298b5091c3cafa3e47f69cc534b0
allainerain <allainericci@gmail.com>
2024-06-26 14:07:04 +0800

    style(Navbar.svelte): update text styes in nav

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="
-    flex fixed w-full items-center justify-center h-[100px] z-50 pt-8
+    flex fixed w-full items-center justify-center h-[100px] z-40 pt-8
     md:top-0
     max-md:bottom-0 max-md:left-0 max-md:right-0 
     bg-gradient-to-t from-light-background to-transparent
@@ -12,13 +12,13 @@
 ">
     <div class="
         inline-flex gap-4 bg-light-background items-center justify-center border m-4 p-4 rounded-lg border-1 border-dark-accent/[0.2]
-        max-md:w-[80%] max-md:ml-[5%]
+        max-md:ml-[5%] max-md:mb-20
         dark:bg-dark-background dark:text-dark-text
     ">
-        <a href="/" class="px-3 py-1"> Home </a>
-        <a href="/about" class="px-3 py-1"> About </a>
+        <a href="/" class="px-3 py-1"><p>Home</p></a>
+        <a href="/about" class="px-3 py-1"><p>About</p></a>
         <ThemeSwitch />
-        <a href="/works" class="px-3 py-1"> Works </a>
-        <a href="/blog" class="px-3 py-1"> Blog </a>
+        <a href="/works" class="px-3 py-1"><p>Works</p></a>
+        <a href="/blog" class="px-3 py-1"><p>Blog</p></a>
     </div>
 </div>
