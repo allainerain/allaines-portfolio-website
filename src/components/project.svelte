be5e5a9b8554e270616e153f27cf8515b3ea0528
allainerain <allainericci@gmail.com>
2024-06-26 14:08:33 +0800

    style(Project.svelte): update project style for popup

--- src/components/project.svelte
+++ src/components/project.svelte
@@ -1,7 +1,7 @@
 <script>
-    import Carousel from "./carousel.svelte";
+    import Carousel from "./Carousel.svelte";
     export let gallery_items;
-    export let title;
+    export let title = "Placeholder";
     export let description;
     export let github;
     export let figma;
@@ -9,145 +9,40 @@
     export let case_study;
 </script>
 
-<section>
-    <h1>{title}</h1>
-    <p>{description}</p>
+<section class="flex flex-col gap-8 w-full h-svh items-center justify-center">
+    <div class="flex flex-col gap-4">
+        <span class="flex flex-col gap-2">
+            <h6>{title}</h6>
+            <p>{description}</p>
+        </span>
 
-    <div>
-        {#if case_study != "none"}
-        <a href={case_study}>
-            <button type="button">
-                <span class="button-text">
-                    <p class="case-study">Case Study</p>
-                    <img src="/icons/arrow-top-right.png" alt="arrow"/>
-                </span>
-            </button>
-        </a>
-        {/if}
+        <!-- Case Study Link -->
+        <div>
+            {#if case_study != "none"}
+            <a href={case_study}>
+                <button type="button">
+                    <span class="button-text">
+                        <p class="case-study">Case Study</p>
+                        <img src="/icons/arrow-top-right.png" alt="arrow"/>
+                    </span>
+                </button>
+            </a>
+            {/if}
+        </div>
+
+        <!-- Project Links -->
+        <span class="links">
+            {#if github != "none"}
+                <a href={github} target="_blank">Github</a>
+            {/if}
+            {#if figma != "none"}
+                <a href={figma} target="_blank">Figma</a>
+            {/if}
+            {#if live != "none"}
+                <a href={live} target="_blank">Live</a>
+            {/if}
+        </span>
     </div>
-
-    <span class="links">
-        {#if github != "none"}
-            <a href={github} target="_blank">Github</a>
-        {/if}
-        {#if figma != "none"}
-            <a href={figma} target="_blank">Figma</a>
-        {/if}
-        {#if live != "none"}
-            <a href={live} target="_blank">Live</a>
-        {/if}
-    </span>
-    
     <Carousel gallery_items={gallery_items}/>
+
 </section>
-
-<style>
-    section {
-        color: #C2C2C2;
-        width: 800px;
-        margin: 100px auto 100px auto;
-        text-align: center;
-    }
-
-    button {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border-width: 1px;
-        border-color: #838481;
-        border-style: solid;
-        background-color: #161A11;
-        padding: 10px 20px;
-        border-radius: 100px;
-        margin: 20px auto; /* Center the button horizontally */
-        font-size: inherit;
-        line-height: inherit;
-        cursor: pointer;
-    }
-
-    button:hover {
-        background-color: #1E2319;
-        /* cursor: pointer; */
-    }
-
-    .button-text {
-        display: flex;
-        align-items: center;
-        gap: 5px;
-    }
-
-    .case-study{
-        color: #C2C2C2;
-        margin: 0;
-        font-family: 'Roboto Mono', monospace;
-        font-size: 18px;
-    }
-
-    h1{
-        font-family: 'Hogbine', sans-serif;
-        font-weight: 400;
-        font-size: 96px;
-        line-height: 1.11;
-        margin: 24px;
-    }
-
-    p{
-        font-family: 'Satoshi', sans-serif;
-        font-size: 18px;
-        line-height: 1.75;
-        color: #B3B3B3
-    }
-
-    a{
-        font-family: 'Roboto Mono', monospace;
-        font-size: 18px;
-        text-decoration: none;
-        margin: 0 10px;
-        color: #747474;
-        transition: color 0.3s ease; 
-    }
-
-    a:hover{
-        color:  #C2C2C2;
-    }
-
-    /* TABLET VIEW */
-    @media only screen and (max-width: 900px) {
-        section{
-            width: 90%;
-            margin: 75px 3% 75px 3%; /* Adjust the margin for small screens */
-        }
-        h1{
-            font-size: 72px;
-        }
-        p{
-            font-size: 20px;
-        }
-        .case-study{
-            font-size: 16px;
-        }
-        a{
-            font-size: 16px;
-        }
-    }
-
-    /* PHONE VIEW */
-    @media only screen and (max-width:770px){
-        section{
-            width: 90%;
-            margin: 50px 5% 50px 5%; /* Adjust the margin for small screens */
-        }
-        h1{
-            font-size: 54px;
-        }
-        p{
-            font-size: 16px;
-        }
-        .case-study{
-            font-size: 14px;
-        }
-        a{
-            font-size: 16px;
-        }
-    }
-</style>
