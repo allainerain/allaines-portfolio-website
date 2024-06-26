846deef0e341e76b9412d0abcf8db4b985c0ee2d
allainerain <allainericci@gmail.com>
2024-06-26 14:30:03 +0800

    feat: add Divider.svelte component

---
+++ src/components/Divider.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let text;
+</script>
+
+<div class="grid gap-[12px]">
+    <hr class="w-full border-t-2 dark:border-light-border/[.40]">
+    <p class="text-light-accent dark:text-dark-accent">{text}</p>
+</div>
