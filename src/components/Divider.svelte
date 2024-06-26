93f054d1fff130807777f60e9361d1716f21f859
allainerain <allainericci@gmail.com>
2024-06-26 15:36:51 +0800

    style(Divider.svelte): adjust gap

--- src/components/Divider.svelte
+++ src/components/Divider.svelte
@@ -2,7 +2,7 @@
     export let text;
 </script>
 
-<div class="grid gap-[12px]">
+<div class="grid gap-5">
     <hr class="w-full border-t-2 dark:border-light-border/[.40]">
-    <p class="text-light-accent dark:text-dark-accent">{text}</p>
+    <p class="text-light-border dark:text-dark-accent">{text}</p>
 </div>
