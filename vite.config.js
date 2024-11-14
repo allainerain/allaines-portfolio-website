7af61a3ef8fc955854e022cf5be0cb8d478012fd
Allaine Tan <56602966+allainerain@users.noreply.github.com>
2024-11-15 00:42:40 +0800

    deps: update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import './dotenv-config.js';
-
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import "./dotenv-config.js";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
 });
